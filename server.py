e9e8163c8d24c117229405310974172ec75861bf
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2022-11-05 11:57:51 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,43 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+with open("/var/run/argo/token") as f:
+    token = f.read()
+
+
+class Plugin(BaseHTTPRequestHandler):
+
+    def args(self):
+        return json.loads(self.rfile.read(int(self.headers.get('Content-Length'))))
+
+    def reply(self, reply):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps(reply).encode("UTF-8"))
+
+    def forbidden(self):
+        self.send_response(403)
+        self.end_headers()
+
+    def unsupported(self):
+        self.send_response(404)
+        self.end_headers()
+
+    def do_POST(self):
+        if self.headers.get("Authorization") != "Bearer " + token:
+            self.forbidden()
+        elif self.path == '/api/v1/template.execute':
+            args = self.args()
+            if 'hello' in args['template'].get('plugin', {}):
+                self.reply(
+                    {'node': {'phase': 'Succeeded', 'message': 'Hello template!',
+                              'outputs': {'parameters': [{'name': 'foo', 'value': 'bar'}]}}})
+            else:
+                self.reply({})
+        else:
+            self.unsupported()
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('', 4355), Plugin)
+    httpd.serve_forever()
